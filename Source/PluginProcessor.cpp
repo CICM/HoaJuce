f534199f34f7f0ee37f2fc4b06772b5947ace3c6
pierreguillot <guillotpierre6@gmail.com>
2017-02-15 11:32:15 +0100

    update processor

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -9,124 +9,60 @@
 */
 
 #include "PluginProcessor.h"
-#include "PluginEditor.h"
 
 
-HoaAudioProcessor::HoaAudioProcessor()
-#ifndef JucePlugin_PreferredChannelConfigurations
-     : AudioProcessor (BusesProperties()
-                     #if ! JucePlugin_IsMidiEffect
-                      #if ! JucePlugin_IsSynth
-                       .withInput  ("Input",  AudioChannelSet::stereo(), true)
-                      #endif
-                       .withOutput ("Output", AudioChannelSet::stereo(), true)
-                     #endif
-                       )
-#endif
+HoaAudioProcessor::HoaAudioProcessor() :
+m_encoder(JucePlugin_HoaOrder, JucePlugin_HoaNumSources),
+m_optim(JucePlugin_HoaOrder),
+m_decoder(JucePlugin_HoaOrder, JucePlugin_HoaNumChannels)
 {
-    m_encoder = new hoa::Encoder<hoa::Hoa3d, float>::Multi(m_order, m_nsources);
-    m_optim   = new hoa::Optim<hoa::Hoa3d, float>::InPhase(m_order);
-    m_decoder = new hoa::Decoder<hoa::Hoa3d, float>::Regular(m_order, m_nhps);
-    
     // 0 0 45 0 90 0 135 0 180 0 225 0 270 0 315 0 335 38 47 38 119 38 191 38 263 38 287 75 47 75 167 75
-    m_decoder->setPlanewaveAzimuth(0, 0.);
-    m_decoder->setPlanewaveAzimuth(1, 45.);
-    m_decoder->setPlanewaveAzimuth(2, 90.);
-    m_decoder->setPlanewaveAzimuth(3, 135);
-    m_decoder->setPlanewaveAzimuth(4, 180.);
-    m_decoder->setPlanewaveAzimuth(5, 225.);
-    m_decoder->setPlanewaveAzimuth(6, 270.);
-    m_decoder->setPlanewaveAzimuth(7, 315.);
-    m_decoder->setPlanewaveAzimuth(8, 335.);
-    m_decoder->setPlanewaveAzimuth(9, 47.);
-    m_decoder->setPlanewaveAzimuth(10, 119.);
-    m_decoder->setPlanewaveAzimuth(11, 191.);
-    m_decoder->setPlanewaveAzimuth(12, 263.);
-    m_decoder->setPlanewaveAzimuth(13, 287.);
-    m_decoder->setPlanewaveAzimuth(14, 47.);
-    m_decoder->setPlanewaveAzimuth(15, 167.);
+    m_decoder.setPlanewaveAzimuth(0, 0.);
+    m_decoder.setPlanewaveAzimuth(1, 45.);
+    m_decoder.setPlanewaveAzimuth(2, 90.);
+    m_decoder.setPlanewaveAzimuth(3, 135);
+    m_decoder.setPlanewaveAzimuth(4, 180.);
+    m_decoder.setPlanewaveAzimuth(5, 225.);
+    m_decoder.setPlanewaveAzimuth(6, 270.);
+    m_decoder.setPlanewaveAzimuth(7, 315.);
+    m_decoder.setPlanewaveAzimuth(8, 335.);
+    m_decoder.setPlanewaveAzimuth(9, 47.);
+    m_decoder.setPlanewaveAzimuth(10, 119.);
+    m_decoder.setPlanewaveAzimuth(11, 191.);
+    m_decoder.setPlanewaveAzimuth(12, 263.);
+    m_decoder.setPlanewaveAzimuth(13, 287.);
+    m_decoder.setPlanewaveAzimuth(14, 47.);
+    m_decoder.setPlanewaveAzimuth(15, 167.);
     
-    m_decoder->setPlanewaveElevation(0, 0.);
-    m_decoder->setPlanewaveElevation(1, 0.);
-    m_decoder->setPlanewaveElevation(2, 0.);
-    m_decoder->setPlanewaveElevation(3, 0.);
-    m_decoder->setPlanewaveElevation(4, 0.);
-    m_decoder->setPlanewaveElevation(5, 0.);
-    m_decoder->setPlanewaveElevation(6, 0.);
-    m_decoder->setPlanewaveElevation(7, 0.);
-    m_decoder->setPlanewaveElevation(8, 38.);
-    m_decoder->setPlanewaveElevation(9, 38.);
-    m_decoder->setPlanewaveElevation(10, 38.);
-    m_decoder->setPlanewaveElevation(11, 38.);
-    m_decoder->setPlanewaveElevation(12, 38.);
-    m_decoder->setPlanewaveElevation(13, 75.);
-    m_decoder->setPlanewaveElevation(14, 75.);
-    m_decoder->setPlanewaveElevation(15, 75.);
+    m_decoder.setPlanewaveElevation(0, 0.);
+    m_decoder.setPlanewaveElevation(1, 0.);
+    m_decoder.setPlanewaveElevation(2, 0.);
+    m_decoder.setPlanewaveElevation(3, 0.);
+    m_decoder.setPlanewaveElevation(4, 0.);
+    m_decoder.setPlanewaveElevation(5, 0.);
+    m_decoder.setPlanewaveElevation(6, 0.);
+    m_decoder.setPlanewaveElevation(7, 0.);
+    m_decoder.setPlanewaveElevation(8, 38.);
+    m_decoder.setPlanewaveElevation(9, 38.);
+    m_decoder.setPlanewaveElevation(10, 38.);
+    m_decoder.setPlanewaveElevation(11, 38.);
+    m_decoder.setPlanewaveElevation(12, 38.);
+    m_decoder.setPlanewaveElevation(13, 75.);
+    m_decoder.setPlanewaveElevation(14, 75.);
+    m_decoder.setPlanewaveElevation(15, 75.);
 }
 
 HoaAudioProcessor::~HoaAudioProcessor()
 {
 }
 
-//==============================================================================
-const String HoaAudioProcessor::getName() const
-{
-    return JucePlugin_Name;
-}
-
-bool HoaAudioProcessor::acceptsMidi() const
-{
-   #if JucePlugin_WantsMidiInput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-bool HoaAudioProcessor::producesMidi() const
-{
-   #if JucePlugin_ProducesMidiOutput
-    return true;
-   #else
-    return false;
-   #endif
-}
-
-double HoaAudioProcessor::getTailLengthSeconds() const
-{
-    return 0.0;
-}
-
-int HoaAudioProcessor::getNumPrograms()
-{
-    return 1;   // NB: some hosts don't cope very well if you tell them there are 0 programs,
-                // so this should be at least 1, even if you're not really implementing programs.
-}
-
-int HoaAudioProcessor::getCurrentProgram()
-{
-    return 0;
-}
-
-void HoaAudioProcessor::setCurrentProgram (int index)
-{
-}
-
-const String HoaAudioProcessor::getProgramName (int index)
-{
-    return String();
-}
-
-void HoaAudioProcessor::changeProgramName (int index, const String& newName)
-{
-}
 
 //==============================================================================
 void HoaAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    m_inputs    = new float[samplesPerBlock * m_nsources];
-    m_harmonics = new float[samplesPerBlock * m_nharmos];
-    m_outputs   = new float[samplesPerBlock * m_nhps];
+    m_inputs    = new float[samplesPerBlock * JucePlugin_HoaNumSources];
+    m_harmonics = new float[samplesPerBlock * JucePlugin_HoaNumHarmonics];
+    m_outputs   = new float[samplesPerBlock * JucePlugin_HoaNumChannels];
 }
 
 void HoaAudioProcessor::releaseResources()
@@ -136,72 +72,36 @@ void HoaAudioProcessor::releaseResources()
     delete [] m_outputs;
 }
 
-#ifndef JucePlugin_PreferredChannelConfigurations
-bool HoaAudioProcessor::isBusesLayoutSupported (const BusesLayout& layouts) const
-{
-  #if JucePlugin_IsMidiEffect
-    ignoreUnused (layouts);
-    return true;
-  #else
-    // This is the place where you check if the layout is supported.
-    // In this template code we only support mono or stereo.
-    if (layouts.getMainOutputChannelSet() != AudioChannelSet::mono()
-     && layouts.getMainOutputChannelSet() != AudioChannelSet::stereo())
-        return false;
-
-    // This checks if the input layout matches the output layout
-   #if ! JucePlugin_IsSynth
-    if (layouts.getMainOutputChannelSet() != layouts.getMainInputChannelSet())
-        return false;
-   #endif
-
-    return true;
-  #endif
-}
-#endif
-
 void HoaAudioProcessor::processBlock (AudioSampleBuffer& buffer, MidiBuffer& midiMessages)
 {
     const int nins  = getTotalNumInputChannels();
     const int nouts = getTotalNumOutputChannels();
-    const int nharmos = m_nharmos;
     const int nsamples = buffer.getNumSamples();
 
     
     for(int i = 0; i < nins; ++i)
     {
-        hoa::Signal<float>::copy(nsamples, buffer.getReadPointer(i), 1, m_inputs+i, nins);
+        hoa::Signal<float>::copy(nsamples, buffer.getReadPointer(i), 1, m_inputs+i, JucePlugin_HoaNumSources);
     }
     for(int i = 0; i < nsamples; i++)
     {
         for(int j = 0; j < nins; ++j)
         {
             // Here you can change the radius, the azimuth and the elevation for each source j
-            m_encoder->setRadius(j, 1.);
-            m_encoder->setAzimuth(j, 0.);
-            m_encoder->setElevation(j, 0.);
+            m_encoder.setRadius(j, 1.);
+            m_encoder.setAzimuth(j, 0.);
+            m_encoder.setElevation(j, 0.);
         }
-        m_encoder->process(m_inputs+nins*i, m_harmonics+nharmos*i);
-        m_optim->process(m_harmonics+nharmos*i, m_harmonics+nharmos*i);
-        m_decoder->process(m_harmonics+nharmos*i, m_outputs+nouts*i);
+        m_encoder.process(m_inputs+JucePlugin_HoaNumSources*i, m_harmonics+JucePlugin_HoaNumHarmonics*i);
+        m_optim.process(m_harmonics+JucePlugin_HoaNumHarmonics*i, m_harmonics+JucePlugin_HoaNumHarmonics*i);
+        m_decoder.process(m_harmonics+JucePlugin_HoaNumHarmonics*i, m_outputs+JucePlugin_HoaNumHarmonics*i);
     }
     
     for(long i = 0; i < nouts; i++)
     {
-        hoa::Signal<float>::copy(nsamples, m_outputs+i, nouts, buffer.getWritePointer(i), 1);
+        hoa::Signal<float>::copy(nsamples, m_outputs+i, JucePlugin_HoaNumChannels, buffer.getWritePointer(i), 1);
     }
     
-}
-
-//==============================================================================
-bool HoaAudioProcessor::hasEditor() const
-{
-    return true; // (change this to false if you choose to not supply an editor)
-}
-
-AudioProcessorEditor* HoaAudioProcessor::createEditor()
-{
-    return new HoaAudioProcessorEditor (*this);
 }
 
 //==============================================================================
--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -25,47 +25,32 @@ public:
     HoaAudioProcessor();
     ~HoaAudioProcessor();
 
-    //==============================================================================
     void prepareToPlay (double sampleRate, int samplesPerBlock) override;
     void releaseResources() override;
 
-   #ifndef JucePlugin_PreferredChannelConfigurations
-    bool isBusesLayoutSupported (const BusesLayout& layouts) const override;
-   #endif
-
     void processBlock (AudioSampleBuffer&, MidiBuffer&) override;
 
-    //==============================================================================
-    AudioProcessorEditor* createEditor() override { return nullptr; }
-    bool hasEditor() const override { return false; }
-
-    //==============================================================================
-    const String getName() const override;
-
-    bool acceptsMidi() const override;
-    bool producesMidi() const override;
-    double getTailLengthSeconds() const override;
-
-    //==============================================================================
-    int getNumPrograms() override;
-    int getCurrentProgram() override;
-    void setCurrentProgram (int index) override;
-    const String getProgramName (int index) override;
-    void changeProgramName (int index, const String& newName) override;
+    AudioProcessorEditor* createEditor() override   { return nullptr; }
+    bool hasEditor() const override                 { return false; }
+    const String getName() const override           { return JucePlugin_Name; }
+    bool acceptsMidi() const override               { return JucePlugin_WantsMidiInput; }
+    bool producesMidi() const override              { return JucePlugin_ProducesMidiOutput; }
+    double getTailLengthSeconds() const override    { return 0.; }
+    int getNumPrograms() override                   { return 1; }
+    int getCurrentProgram() override                { return 0; }
+    void setCurrentProgram (int index) override     { }
+    const String getProgramName (int index) override{ return String(); }
+    void changeProgramName (int index, const String& newName) override { }
 
     //==============================================================================
     void getStateInformation (MemoryBlock& destData) override;
     void setStateInformation (const void* data, int sizeInBytes) override;
 
 private:
-    static const size_t m_order     = 3;
-    static const size_t m_nsources  = 16;
-    static const size_t m_nhps      = 16;
-    static const size_t m_nharmos   = 16; // (3+1)^2
     
-    ScopedPointer<hoa::Encoder<hoa::Hoa3d, float>::Multi>   m_encoder;
-    ScopedPointer<hoa::Optim<hoa::Hoa3d, float>::InPhase>   m_optim;
-    ScopedPointer<hoa::Decoder<hoa::Hoa3d, float>::Regular> m_decoder;
+    hoa::Encoder<hoa::Hoa3d, float>::Multi      m_encoder;
+    hoa::Optim<hoa::Hoa3d, float>::InPhase      m_optim;
+    hoa::Decoder<hoa::Hoa3d, float>::Regular    m_decoder;
     float*                                      m_inputs;
     float*                                      m_harmonics;
     float*                                      m_outputs;
